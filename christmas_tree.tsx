e0d9fc29eecd981ce0c0495d838aee21d110f1b8
FlyingBabYpsilon <50945991+FlyingBabYpsilon@users.noreply.github.com>
2021-12-02 18:48:59 +0100

    added start and labyrinth
    feat: labyrinth & start

---
+++ christmas_tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="christmas_tree" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="christmas_tree.png" width="256" height="160"/>
+</tileset>
---
+++ winter_floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="winter_floor" tilewidth="32" tileheight="32" tilecount="72" columns="9">
+ <image source="sloppymess.png" width="288" height="256"/>
+</tileset>
